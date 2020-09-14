813201e19a40f82019d4f92238b2ba7860a92700
ljt <742641068@qq.com>
2020-09-14 15:36:58 +0800

    快速排序

---
+++ 5_快速排序.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+
+/*
+基本思想：（分治）
+	先从数列中取出一个数作为key值；
+	将比这个数小的数全部放在它的左边，大于或等于它的数全部放在它的右边；
+	对左右两个小数列重复第二步，直至各区间只有1个数。
+*/
+func quickSort(arr []int) []int {
+	return _quickSort(arr, 0, len(arr)-1)
+}
+func _quickSort(arr []int, left, right int) []int {
+	if left < right {
+		partitionIndex := partition_(arr, left, right)
+		_quickSort(arr, left, partitionIndex-1)
+		_quickSort(arr, partitionIndex+1, right)
+	}
+	return arr
+}
+func partition_(arr []int, left, right int) int {
+	pivot := left
+	index := pivot + 1
+	for i := index; i <= right; i++ {
+		if arr[i] < arr[pivot] {
+			swap(arr, i, index)
+			index += 1
+		}
+	}
+	swap(arr, pivot, index-1)
+	return index - 1
+}
+func swap(arr []int, i, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
+
+
+// 普通快速排序
+func QuickSort(array []int, begin, end int) {
+	if begin < end {
+		// 进行切分
+		loc := partition(array, begin, end)
+		// 对左部分进行快排
+		QuickSort(array, begin, loc-1)
+		// 对右部分进行快排
+		QuickSort(array, loc+1, end)
+	}
+}
+// 切分函数，并返回切分元素的下标
+func partition(array []int, begin, end int) int {
+	i := begin + 1 // 将array[begin]作为基准数，因此从array[begin+1]开始与基准数比较！
+	j := end       // array[end]是数组的最后一位
+	// 没重合之前
+	for i < j {
+		if array[i] > array[begin] {
+			array[i], array[j] = array[j], array[i] // 交换
+			j--
+		} else {
+			i++
+		}
+	}
+	/* 跳出while循环后，i = j。
+	 * 此时数组被分割成两个部分  -->  array[begin+1] ~ array[i-1] < array[begin]
+	 *                        -->  array[i+1] ~ array[end] > array[begin]
+	 * 这个时候将数组array分成两个部分，再将array[i]与array[begin]进行比较，决定array[i]的位置。
+	 * 最后将array[i]与array[begin]交换，进行两个分割部分的排序！以此类推，直到最后i = j不满足条件就退出！
+	 */
+	if array[i] >= array[begin] { // 这里必须要取等“>=”，否则数组元素由相同的值组成时，会出现错误！
+		i--
+	}
+	array[begin], array[i] = array[i], array[begin]
+	return i
+}
+func main() {
+	list := []int{5}
+	QuickSort(list, 0, len(list)-1)
+	fmt.Println(list)
+	list1 := []int{5, 9}
+	QuickSort(list1, 0, len(list1)-1)
+	fmt.Println(list1)
+	list2 := []int{5, 9, 1}
+	QuickSort(list2, 0, len(list2)-1)
+	fmt.Println(list2)
+	list3 := []int{5, 9, 1, 6, 8, 14, 6, 49, 25, 4, 6, 3}
+	QuickSort(list3, 0, len(list3)-1)
+	fmt.Println(list3)
+}
